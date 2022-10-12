5d563d3723cf632f48d6a1c5a86acd09d859043f
storytellerF <34095089+storytellerF@users.noreply.github.com>
2022-10-12 22:23:10 +0800

    从service 中启动

--- app/src/main/java/com/storyteller_f/kuang/KuangService.kt
+++ app/src/main/java/com/storyteller_f/kuang/KuangService.kt
@@ -5,21 +5,55 @@ import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
 
 class KuangService : Service() {
-
+    private var binder: Kuang? = null
     override fun onBind(intent: Intent): IBinder {
         Log.d(TAG, "onBind() called with: intent = $intent")
-        return Kuang()
+        return Kuang().also {
+            binder = it
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val s = "foreground"
+        val channel = NotificationChannelCompat.Builder(s, NotificationManagerCompat.IMPORTANCE_MIN).apply {
+            setName("running")
+            setSound(null, null)
+        }.build()
+        val from = NotificationManagerCompat.from(this)
+        if (from.getNotificationChannel(s) == null)
+            from.createNotificationChannel(channel)
+        val notification = NotificationCompat.Builder(this, s).apply {
+            setSound(null)
+        }.build()
+        startForeground(foreground_notification_id, notification)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopForeground(true)
+        binder?.stop()
     }
 
     class Kuang : Binder() {
+        private var server: ApplicationEngine? = null
         fun start() {
-
+            server = embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+                configureRouting()
+            }.start(wait = false)
         }
+
         fun stop() {
-
+            server?.stop()
         }
+
         fun restart() {
             stop()
             start()
@@ -28,5 +62,6 @@ class KuangService : Service() {
 
     companion object {
         private const val TAG = "KuangService"
+        private const val foreground_notification_id = 10
     }
 }
--- app/src/main/java/com/storyteller_f/kuang/MainActivity.kt
+++ app/src/main/java/com/storyteller_f/kuang/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.storyteller_f.kuang
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,6 +22,7 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import java.lang.ref.WeakReference
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,11 +35,33 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-        embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-            configureRouting()
-        }.start(wait = false)
+
+        //连接服务
+        try {
+            bindService(Intent(this, KuangService::class.java), connection, BIND_AUTO_CREATE)
+        } catch (e: Exception) {
+            bindService(Intent(this, KuangService::class.java), connection, 0)
+        }
     }
 
+    var fileOperateBinder: KuangService.Kuang? = null
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            Toast.makeText(this@MainActivity, "服务已连接", Toast.LENGTH_SHORT).show()
+            val fileOperateBinderLocal = service as KuangService.Kuang
+            Log.i(TAG, "onServiceConnected: $fileOperateBinderLocal")
+            fileOperateBinder = fileOperateBinderLocal
+            fileOperateBinderLocal.start()
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            Toast.makeText(this@MainActivity, "服务已关闭", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 
 }
 
